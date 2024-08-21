bdede99bfd68dc68d186a51565b564f59f1ba58e
KSU <stpd.sonik@mail.ru>
2024-08-21 21:10:29 +0300

    init

---
+++ src/main/java/com/example/wantedtestassigment/WantedTestAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.wantedtestassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedTestAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedTestAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wantedtestassigment/WantedTestAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wantedtestassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedTestAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
